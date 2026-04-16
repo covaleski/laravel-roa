09deffa32df8c6bbb5dcbc8750cc1d428b7aa14a
covaleski <covaleski23@gmail.com>
2026-04-16 20:36:28 -0300

    refactor: improve compilation performance

--- custom/Resource/ModelCompiler.php
+++ custom/Resource/ModelCompiler.php
@@ -9,14 +9,21 @@ use ReflectionClass;
 class ModelCompiler
 {
     /**
+     * Reflection class instance.
+     */
+    final protected ReflectionClass $reflection;
+
+    /**
      * Compile the specified model class to a resource instance.
      */
     public function compile(string $class_name): Resource
     {
-        $resource = new Resource();
-        $resource->name = $this->generateName($class_name);
-        $resource->model = $class_name;
-        return $resource;
+        return $this->compiling($class_name, function () use ($class_name) {
+            $resource = new Resource();
+            $resource->name = $this->compileName($class_name);
+            $resource->model = $class_name;
+            return $resource;
+        });
     }
 
     /**
@@ -24,20 +31,33 @@ class ModelCompiler
      */
     public function compileName(string $class_name): string
     {
-        $reflection = new ReflectionClass($class_name);
-        $attributes = $reflection->getAttributes(ResourceName::class);
-        if (isset($attributes[0])) {
-            return $attributes[0]->newInstance()->name;
-        } else {
-            return $this->generateName($class_name);
-        }
+        return $this->compiling($class_name, function () use ($class_name) {
+            $attributes = $this->reflection->getAttributes(ResourceName::class);
+            return isset($attributes[0])
+                ? $attributes[0]->newInstance()->name
+                : Str::plural(Str::kebab(class_basename($class_name)));
+        });
     }
 
     /**
-     * Generate a resource name for a model class.
+     * Run the specified compilation callback for the specified class.
+     *
+     * @template TResult
+     * @param class-string<Model> $class_name
+     * @param callable(): TResult $callback
+     * @return TResult
      */
-    protected function generateName(string $class_name): string
+    protected function compiling(string $class_name, callable $callback): mixed
     {
-        return Str::plural(Str::kebab(class_basename($class_name)));
+        if (isset($this->reflection)) {
+            return call_user_func($callback);
+        } else {
+            try {
+                $this->reflection = new ReflectionClass($class_name);
+                return call_user_func($callback);
+            } finally {
+                unset($this->reflection);
+            }
+        }
     }
 }
