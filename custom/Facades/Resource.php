4f30b0d162774b81ed356f3f2ac03f874362c201
covaleski <covaleski23@gmail.com>
2026-04-19 15:08:09 -0300

    docs: update `Resource` facade proxied members

--- custom/Facades/Resource.php
+++ custom/Facades/Resource.php
@@ -5,12 +5,22 @@ namespace Covaleski\LaravelRoa\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static array<string, \Covaleski\LaravelRoa\Resource\ResourceAccessor> all() Get all mapped resources.
- * @method static void each(callable(\Covaleski\LaravelRoa\Resource\ResourceAccessor $resource, string $name): void $callback): void Execute a callback over each resource.
- * @method static bool exists(string $name) Check whether a resource is mapped.
- * @method static \Covaleski\LaravelRoa\Resource\ResourceAccessor get(string $name) Get a resource by its unique name.
+ * @method array<string, \Covaleski\LaravelRoa\Resource\ResourceAccessor> all() Get all mapped resources.
+ * @method void cache() Ensure map data is in storage.
+ * @method void clear() Clear map data from memory and storage.
+ * @method void compile() Compile map data to memory.
+ * @method void delete() Delete map data from storage.
+ * @method void each(callable $callback) Execute a callback over each mapped resource.
+ * @method bool exists(string $name) Check whether a resource is mapped.
+ * @method Covaleski\LaravelRoa\Resource\ResourceAccessor get(string $name) Get a resource by its name.
+ * @method bool isCached() Check whether map data is in storage.
+ * @method bool isLoaded() Check whether map data is loaded to memory.
+ * @method void load() Load map data from storage.
+ * @method void save() Save map data from memory to storage.
+ * @method void unload() Clear map data from memory.
+ * @method void wipe() Clear map and resource cache data from memory and storage.
  *
- * @see \Covaleski\LaravelRoa\Resource\ResourceMap
+ * @uses Covaleski\LaravelRoa\Resource\ResourceMap to proxy its members.
  */
 class Resource extends Facade
 {
