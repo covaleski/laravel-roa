039ac1b0aab30471fd6927d59f95a9a020ea0d9c
covaleski <covaleski23@gmail.com>
2026-04-19 23:10:09 -0300

    fix: update the `Resource` facade doc block

--- custom/Facades/Resource.php
+++ custom/Facades/Resource.php
@@ -5,20 +5,20 @@ namespace Covaleski\LaravelRoa\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method array<string, \Covaleski\LaravelRoa\Resource\ResourceAccessor> all() Get all mapped resources.
- * @method void cache() Ensure map data is in storage.
- * @method void clear() Clear map data from memory and storage.
- * @method void compile() Compile map data to memory.
- * @method void delete() Delete map data from storage.
- * @method void each(callable $callback) Execute a callback over each mapped resource.
- * @method bool exists(string $name) Check whether a resource is mapped.
- * @method Covaleski\LaravelRoa\Resource\ResourceAccessor get(string $name) Get a resource by its name.
- * @method bool isCached() Check whether map data is in storage.
- * @method bool isLoaded() Check whether map data is loaded to memory.
- * @method void load() Load map data from storage.
- * @method void save() Save map data from memory to storage.
- * @method void unload() Clear map data from memory.
- * @method void wipe() Clear map and resource cache data from memory and storage.
+ * @method static array<string, \Covaleski\LaravelRoa\Resource\ResourceAccessor> all() Get all mapped resources.
+ * @method static void cache() Ensure map data is in storage.
+ * @method static void clear() Clear map data from memory and storage.
+ * @method static void compile() Compile map data to memory.
+ * @method static void delete() Delete map data from storage.
+ * @method static void each(callable $callback) Execute a callback over each mapped resource.
+ * @method static bool exists(string $name) Check whether a resource is mapped.
+ * @method static \Covaleski\LaravelRoa\Resource\ResourceAccessor get(string $name) Get a resource by its name.
+ * @method static bool isCached() Check whether map data is in storage.
+ * @method static bool isLoaded() Check whether map data is loaded to memory.
+ * @method static void load() Load map data from storage.
+ * @method static void save() Save map data from memory to storage.
+ * @method static void unload() Clear map data from memory.
+ * @method static void wipe() Clear map and resource cache data from memory and storage.
  *
  * @uses Covaleski\LaravelRoa\Resource\ResourceMap to proxy its members.
  */
